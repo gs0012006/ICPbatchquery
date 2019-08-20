51e58c594317cb7fa9ad13451871ccf43d0db579
gs0012006 <88613312@qq.com>
2019-08-20 16:34:30 +0800

    connection

--- src/batchquery/Main.java
+++ src/batchquery/Main.java
@@ -1,13 +1,34 @@
 package batchquery;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		JFrame mainUIFrame;
+
+String filepath ="https://pmmomo.xyz:456/2.txt";
+try {
+    URL pathUrl = new URL(filepath);
+    HttpURLConnection urlcon = (HttpURLConnection) pathUrl.openConnection();
+    if(urlcon.getResponseCode()>=400){ 
+       System.out.println("文件不存在");
+       JOptionPane.showMessageDialog(null,"连接失败",null, 1);
+       System.exit(1);
+    }else{
+       System.out.println("文件存在");
+       JFrame mainUIFrame;
 		mainUIFrame = new mainUI();
+   }
+}catch (Exception e) {
+    System.out.print("请求失败");
+} 	
+
+		
 	}
 
 }
